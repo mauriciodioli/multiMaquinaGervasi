6ba918f833cf05f4118439b349d321534b590f6a
Mauricio Dioli <madioli26@hotmail.com>
2025-07-02 09:48:46 +0000

    cambios 5

--- src/controller/autenticacion/login.py
+++ src/controller/autenticacion/login.py
@@ -20,20 +20,6 @@ login = Blueprint("login", __name__)
 
 
 
-MAIL_SERVER = os.getenv("MAIL_SERVER")
-MAIL_PORT = int(os.getenv("MAIL_PORT"))
-MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == "true"
-MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
-
-
-
-
-
-
-
-
 # ========== LOGIN ==========
 @login.route('/login_usuario/', methods=['POST'])
 def login_usuario():
